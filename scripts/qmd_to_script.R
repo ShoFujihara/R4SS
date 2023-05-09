6d8979b2748e8f60e3c9551cfd82f206848652b9
Sho Fujihara <34362698+ShoFujihara@users.noreply.github.com>
2023-05-10 02:56:58 +0900

    test.

--- scripts/qmd_to_script.R
+++ scripts/qmd_to_script.R
@@ -1,5 +1,8 @@
+# パッケージの呼び出し
 pacman::p_load(knitr,
                here)
+
+# qmdファイルからRスクリプトのみを抜き出す
 purl(here("intro.qmd"), output = here("scripts", "intro.R"), documentation = 0)
 purl(here("data.qmd"), output = here("scripts", "data.R"), documentation = 0)
 purl(here("variables.qmd"), output = here("scripts", "variables.R"), documentation = 0)
